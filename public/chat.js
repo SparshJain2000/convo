c829fecb4b090ef7219c45465b5475d8ca0fb984
SparshJain2000 <jainsparsh0801@gmail.com>
2020-05-19 19:14:09 +0530

    scroll up button

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 const socket = io.connect(window.location.hostname),
-    // const socket = io.connect("http://localhost:5000"),
+    // const socket = io.connect("http://localhost:5000"
     message = document.getElementById("message"),
     output = document.getElementById("output"),
     button = document.getElementById("button"),
@@ -8,9 +8,10 @@ const socket = io.connect(window.location.hostname),
     alert = document.getElementById("alert"),
     chat_window = document.getElementById("chat-window"),
     fileInput = document.getElementById("file-input");
-
 let image = "";
-
+$("#up").hide();
+//=================================================================
+//input image
 $("#file-input").on("change", function (e) {
     var file = e.originalEvent.target.files[0],
         reader = new FileReader();
@@ -18,7 +19,10 @@ $("#file-input").on("change", function (e) {
         image = evt.target.result;
     };
     reader.readAsDataURL(file);
+    message.focus();
 });
+//=================================================================
+//send message
 button.addEventListener("click", () => {
     let data = {
         handle: name.textContent,
@@ -29,33 +33,18 @@ button.addEventListener("click", () => {
     message.value = "";
     socket.emit("chat", data);
 });
-message.addEventListener("keyup", () => {
+//=================================================================
+//trigger user typing and send message on enter
+message.addEventListener("keyup", (event) => {
     if (message.value !== "") socket.emit("typing", name.textContent);
     else socket.emit("typing", "stop");
+    //13 => keycode for Enter
+    if (event.keyCode === 13) button.click();
 });
+//=================================================================
+//New User connected
 socket.on("connect", () => {
     socket.emit("newconnection", name.textContent);
-});
-
-socket.on("userDisconnected", (data) => {
-    $("#alert")
-        .html(
-            "<div class='alert alert-danger' role='alert'>" +
-                data +
-                " has left the chat" +
-                "</div>"
-        )
-        .hide();
-    window.setTimeout(function () {
-        $("#alert").slideDown(500);
-    }, 500);
-    window.setTimeout(function () {
-        $(".alert")
-            .fadeTo(500, 0)
-            .slideUp(500, function () {
-                $(this).remove();
-            });
-    }, 3000);
 });
 socket.on("newconnection", (data) => {
     $("#alert")
@@ -66,9 +55,7 @@ socket.on("newconnection", (data) => {
                 "</div>"
         )
         .hide();
-    window.setTimeout(function () {
-        $("#alert").slideDown(500);
-    }, 500);
+    $("#alert").slideDown(500);
     window.setTimeout(function () {
         $(".alert")
             .fadeTo(500, 0)
@@ -77,75 +64,100 @@ socket.on("newconnection", (data) => {
             });
     }, 3000);
 });
+//=================================================================
+//User Disconnected
+socket.on("userDisconnected", (data) => {
+    $("#alert")
+        .html(
+            "<div class='alert alert-danger' role='alert'>" +
+                data +
+                " has left the chat" +
+                "</div>"
+        )
+        .hide();
+    $("#alert").slideDown(500);
+    window.setTimeout(function () {
+        $(".alert")
+            .fadeTo(500, 0)
+            .slideUp(500, function () {
+                $(this).remove();
+            });
+    }, 3000);
+});
+//=================================================================
+//Recieve message from server and show it on client side
 socket.on("chat", (data) => {
-    // console.log(data);
+    let style, bg, color;
+
+    let users = "";
     if (data.handle === name.textContent) {
-        let users = "";
+        style = "style='display:flex;justify-content:flex-end'";
         data.users.forEach((item) => {
             if (item.name.trim() !== data.handle.trim())
                 users += item.name + " , ";
         });
         users = users.slice(0, users.length - 3);
+        bg = "bg-secondary";
+        color = "text-warning";
+    } else {
+        style = "style='display:flex;justify-content:flex-start'";
+        bg = "bg-dark";
+        color = "text-success";
+    }
+    output.innerHTML +=
+        "<div " +
+        style +
+        "><div class='" +
+        bg +
+        " mess p-2 mr-1 m-2 rounded col-8 '><h6 class='" +
+        color +
+        " text-capitalize'>" +
+        data.handle +
+        " </h6><h5> " +
+        data.message +
+        "</h5>" +
+        '<img class="img-fluid rounded" src="' +
+        data.image +
+        '"/>' +
+        "<div style='text-align:right'>" +
+        new Intl.DateTimeFormat("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+        }).format(new Date(data.date)) +
+        "</div></div></div>";
+    if (users !== "")
         output.innerHTML +=
-            "<div style='display:flex;justify-content:flex-end'><div class='bg-secondary mess p-2 mr-1 m-2 rounded col-8 '><h6 class='text-warning text-capitalize'>" +
-            data.handle +
-            " </h6><h5> " +
-            data.message +
-            "</h5>" +
-            '<img class="img-fluid rounded" src="' +
-            data.image +
-            '"/>' +
-            "<div style='text-align:right'>" +
-            new Intl.DateTimeFormat("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-            }).format(new Date(data.date)) +
-            "</div></div></div>";
-
-        output.innerHTML +=
-            "<div style='display:flex;justify-content:flex-end'><div class='bg-success seen pl-2 pr-2 p-1 mr-2 rounded col-8 '><strong><em>Seen by " +
+            "<div " +
+            style +
+            " ><div class='bg-success seen pl-2 pr-2 p-1 mr-2 rounded col-8 '><strong><em>Seen by " +
             users +
             "</em></strong></div></div>";
-    } else {
-        feedback.innerHTML = "";
-        output.innerHTML +=
-            "<div style='display:flex;justify-content:flex-start'><div class='bg-dark mess p-2 ml-1 m-2 rounded col-8 '><h6 class='text-success text-capitalize'>" +
-            data.handle +
-            " </h6><h5> " +
-            data.message +
-            "</h5>" +
-            '<img class="img-fluid rounded" src="' +
-            data.image +
-            '"/>' +
-            "</h5><div style='text-align:right'>" +
-            new Intl.DateTimeFormat("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-            }).format(new Date(data.date)) +
-            "</div></div></div>";
-    }
-    scroll();
+
     $("file-input").val("");
     image = "";
+    socket.emit("typing", "stop");
+    showUp();
+    scroll();
     // message.focus();
 });
+//=================================================================
+//show typing message
 socket.on("typing", (data) => {
-    console.log(data === "stop");
-    if (data === "stop") {
-        $(".type")
-            .fadeTo(500, 0)
-            .slideUp(500, function () {
-                $(this).remove();
-            });
-    } else
+    if (data === "stop") $(".type").remove();
+    else
         $("#feedback").html(
             "<p class='badge badge-info p-2 ml-2 type'><em>" +
                 data +
                 " is typing .... </em></p>"
         );
-
     scroll();
 });
+const showUp = () => {
+    if (chat_window.scrollHeight > chat_window.clientHeight) $("#up").show();
+};
 const scroll = () => {
-    chat_window.scrollTop = chat_window.scrollHeight;
+    chat_window.scrollTo({ top: chat_window.scrollHeight, behavior: "smooth" });
+};
+const scrollUp = () => {
+    chat_window.scrollTo({ top: 0, behavior: "smooth" });
 };
